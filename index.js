4d9a43501778c6035c7b41130318b6d69be9d873
Hiba <eng.lilil92@gmail.com>
2022-08-30 21:59:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer ="III";
+
+
+/*upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
+*/
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"; // according to w3school :If you assign a value to a variable that has not been declared, it will automatically become a GLOBAL variable.
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "RRR";
+}
